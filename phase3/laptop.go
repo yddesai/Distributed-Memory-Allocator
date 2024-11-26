806d7016877342890eb256958ce123bb843fd2c8
Parth Bhalerao <98455586+pvbgeek@users.noreply.github.com>
2024-11-25 22:09:20 -0800

    Update laptop.go

--- phase3/laptop.go
+++ phase3/laptop.go
@@ -43,7 +43,7 @@ type ChunkInfo struct {
 
 var (
     node          Node
-    awsIP         = "<AWS_PUBLIC_IP>" // Replace with your AWS Coordinator's public IP
+    awsIP         = "http://18.144.165.108:8080" // Replace with your AWS Coordinator's public IP
     awsPort       = 8080
     indexFile     = "index.json"
     dataFolder    = "data"
